6797c6e42fd26f5209e900ada1f27bdddbc9e388
Rahmat Noris Adityabrillian <rahmatnoris@gmail.com>
2020-11-12 17:00:51 +0700

    [adit] innit commit

---
+++ src/parts/IconText.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Button from "elements/Button";
+
+export default function IconText() {
+  return (
+    <Button className="brand-text-icon" href="" type="link">
+      PRO<span className="text-gray-900">38</span>
+      {/* <img src="../../public/images/pro38-logo.jpg" alt=""/> */}
+    </Button>
+  );
+}
